164f2f2fb07898f1fdcbfd11b7ef94978b4a8f1e
AdrianRamadhan27 <ramadhanadrian2710@gmail.com>
2022-10-26 22:24:57 +0700

    fix admin register mistake

--- mercatura/admin.py
+++ mercatura/admin.py
@@ -1,4 +1,1 @@
 from django.contrib import admin
-from umkm_module.models import UMKM
-# Register your models here.
-admin.site.register(UMKM)
--- umkm_module/admin.py
+++ umkm_module/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from umkm_module.models import UMKM
 # Register your models here.
+admin.site.register(UMKM)

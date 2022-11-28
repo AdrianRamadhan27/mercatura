f4904cda1abe919dcba7b6b94b6859601e0beb86
AdrianRamadhan27 <ramadhanadrian2710@gmail.com>
2022-11-28 15:15:01 +0700

    Fix CSRF? ini gimana yak help

--- project_django/settings.py
+++ project_django/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = [f'mercatura-id.up.railway.app']
+CSRF_TRUSTED_ORIGINS = [f'{APP_NAME}.up.railway.app']
 
 # Application definition
 

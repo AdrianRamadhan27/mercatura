89aed8691712d2425ca6d2cc82aa0fdfaaa225b9
AdrianRamadhan27 <ramadhanadrian2710@gmail.com>
2022-11-28 16:09:22 +0700

    Fix JSON url

--- kritiksaran_module/urls.py
+++ kritiksaran_module/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('setuju/', setuju_post, name="setuju-post-view"),
     path('total/', total_number, name="total_number"),
     path('anon/total/', total_number_anon, name="total_number_anon"),
-    path('json/,', show_kritiksaran_json, name="show_kritiksaran_json"),
+    path('json/', show_kritiksaran_json, name="show_kritiksaran_json"),
 
 
 ]

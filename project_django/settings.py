06ab7aa0ea203357344e26c3ddb01ca4fa355030
AdrianRamadhan27 <ramadhanadrian2710@gmail.com>
2022-11-28 15:26:43 +0700

    Fix allowed hosts

--- project_django/settings.py
+++ project_django/settings.py
@@ -29,7 +29,7 @@ DEBUG = not PRODUCTION
 
 APP_NAME = os.getenv('APP_NAME', '')
 
-ALLOWED_HOSTS = [f'{APP_NAME}.up.railway.app']
+ALLOWED_HOSTS = ["*"]
 
 if not PRODUCTION:
     ALLOWED_HOSTS += ['.localhost', '127.0.0.1', '[::1]']
